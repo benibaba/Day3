a34a263ea3192c10c413c0f27e9fca11cc21e726
benibaba <65398520+benibaba@users.noreply.github.com>
2020-05-20 16:25:42 +0530

    Update P2.py

--- P2.py
+++ P2.py
@@ -1,12 +1,18 @@
-sizeList = int(input("Enter the no of tuples you want to add in the list: "))
-sizeTuple = int(input("Enter the no of elements you want to add in each tuple: "))
-List = []
-for i in range(sizeList):
-    print("Enter the elements in Tuple", i + 1)
-    Tuple = []
-    for j in range(sizeTuple):
-        Tuple.append(int(input("Enter the element " + str(j + 1) + ": ")))
-    List.append(tuple(Tuple))
-N = int(input("Enter the Nth index about which you want to sort the list: "))
-List.sort(key = lambda x : x[N])
-print("After sorting tuple List by Nth index sort:",List)
+def findSmallest(arr, n):
+    r = 1
+    for i in range(0, n):
+        if arr[i] <= r:
+            r = r + arr[i]
+        else:
+            break
+    return r
+
+
+n1 = int(input("Enter number of elements in list: "))
+arr1 = []
+print("Enter elements of list: ")
+for i in range(n1):
+    c = int(input())
+    arr1.append(c)
+arr1.sort()
+print("Smallest non-representable number: ", findSmallest(arr1, n1))
