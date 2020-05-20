581e64e6e630715c2979758c6616417a789fe353
benibaba <65398520+benibaba@users.noreply.github.com>
2020-05-20 16:24:43 +0530

    Update P1.py

--- P1.py
+++ P1.py
@@ -1,8 +1,4 @@
-size = int(input("Enter the size of tuple: "))
-print("Enter the elements in tuple one by one")
-arr = []
-for i in range(size):
-    arr.append(input())
-arr = tuple(arr)
-element = input("Enter the element whose occurrences you want to know: ")
-print("Tuple contains the element", arr.count(element), "times")
+a = input("Enter a string: ")
+rev = a[::-1]
+print("The reversed string is :")
+print(rev)
